414482177824a1d66dd5719bf8b77e07bd85558b
Zainab990 <zainabomar998@gmail.com>
2023-08-10 17:53:37 -0700

    advanced task b

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+/**
+* main - Entry point
+*
+* Descriptio: C program print without put of printf
+*
+* Return: 1 (Success)
+*/
+
+int main(void)
+{
+	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, message, 59);
+
+	return (1);
+}
