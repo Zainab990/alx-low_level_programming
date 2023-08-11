ed46f24d63e01e0729ea754bccea1dfd52adbad0
Zainab990 <zainabomar998@gmail.com>
2023-08-11 15:46:39 -0700

    advanced_task102_code change

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,16 +17,19 @@ int main(void)
 	{
 	for (num2 = num1; num2 < 100; num2++)
 	{
+		if (num1 != num2)
+		{
+			if (num1 != 0 || num2 != 1)
+				{
+				putchar(',');
+				putchar(' ');
+				}
+
 		putchar((num1 / 10) + '0');
 		putchar((num1 % 10) + '0');
 		putchar(' ');
 		putchar((num2 / 10) + '0');
 		putchar((num2 % 10) + '0');
-
-		if (num1 != 99 || num2 != 99)
-		{
-		putchar(',');
-		putchar(' ');
 		}
 	}
 	}
