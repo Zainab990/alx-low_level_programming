561b78c31a13a9cd48be30d4addc575247694b9b
Zainab990 <zainabomar998@gmail.com>
2023-08-11 14:16:34 -0700

    ‘script’

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,21 +12,22 @@ int main(void)
 {
 	char lowercase = 'a';
 
-		while (lowercase <= 'z');
+		while (lowercase <= 'z')
+
 	{
 		putchar(lowercase);
 		lowercase++;
 	}
-	
+
 	char uppercase = 'A';
 
 		while (uppercase <= 'Z')
-       	{
+	{
 		putchar(uppercase);
-	       	uppercase++;
+		uppercase++;
 	}
 
 		putchar('\n');
-	
+
 	return (0);
 }
