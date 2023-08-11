dd0b3fd4a0da3e6dbbcf50173950cb83fb973a9f
Zainab990 <zainabomar998@gmail.com>
2023-08-11 05:01:25 -0700

    task no.0_betty

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
  * main - Entry point
  *
- * Description: program will assign a random number to a variable 
+ * Description: program will assign a random number to a variable
  *
  * Return: 0 (Success)
 */
@@ -16,11 +16,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0)	
-		printf ("%i is positive\n",n);
+	if (n > 0)
+		printf("%i is positive\n", n);
 	else if (n == 0)
-		printf ("%i is zero\n",n);
+		printf("%i is zero\n", n);
 	else
-		printf ("%i is negative\n",n);
+		printf("%i is negative\n", n);
 	return (0);
 }
