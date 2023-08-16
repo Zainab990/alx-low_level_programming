2212784e66635c64bd3812eabe5ca4d6320e1ee1
Zainab990 <zainabomar998@gmail.com>
2023-08-16 11:27:43 -0700

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,21 @@
 #include "main.h"
 
 /**
- * _putchar - Writes a character to the standard output
- * @c: The character to write
+ * main - Entry point
+ *
+ * Description: _putchar - print_alphabet
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 
+
+
+/**
+ * print_alphabet - Prints the lowercase alphabet followed by a new line
+ *
+ * Description: This function prints the lowercase alphabet using _putchar function.
+ */
 void print_alphabet(void)
 {
 	char letter;
