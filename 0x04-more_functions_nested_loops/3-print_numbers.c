bd549b4268806502120f6ab5f89e9d982101590f
Zainab990 <zainabomar998@gmail.com>
2023-08-17 11:27:51 -0700

    task3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_number - printing 0 - 9
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_number(void)
+{
+	int num = 0;
+
+	do {
+		_putchar(num + 48);
+		num++;
+	} while (num >= 0 && num <= 9);
+	_putchar('\n');
+}
